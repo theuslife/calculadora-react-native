622b34e6234ba6235bd0e8ef55ca410b6db3de4f
Matheus Rodrigues <matheusrodrigues@MacBook-Pro-de-Fernando.local>
2018-10-25 14:22:15 -0300

    scop

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ class Button extends Component {
     if(props.flexValue)
         flexValue = parseInt(this.props.flexValue);
 
-
     let bgColor = '#E0E0E0';
     if(props.bgColor){
       bgColor = props.bgColor
@@ -32,7 +31,7 @@ class Button extends Component {
           borderColor: '#999999'
       },
       txt: {
-          fontSize: 15,
+          fontSize: 18,
           color: colorText,
           fontWeight: 'bold'
       }
@@ -43,7 +42,7 @@ class Button extends Component {
 
   render() {
     return(
-      <TouchableOpacity style={this.style.areaButton}>
+      <TouchableOpacity style={this.style.areaButton} onPress={this.props.onPress}>
           <Text style={this.style.txt}>{this.props.text}</Text>
       </TouchableOpacity>
     )
@@ -51,40 +50,67 @@ class Button extends Component {
 
 }
 
-
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      result: '0'
+    }
+
+    this.calculation = this.calculation.bind(this);
+
+  }
+
+  calculation(btn){
+    let state = this.state;
+    if(btn == 'AC'){2
+      state.result = '0' 
+    } else if (btn == '='){
+      state.result = eval(state.result);
+    } else {
+      if(state.result == '0'){
+        state.result = btn;
+      } else {
+        state.result += btn;
+      }
+    }
+
+    this.setState(state);
+  }
+
   render() {
     return (
       <View style={styles.body}>
           <View style={styles.row}>
-            <Text></Text>
+            <Text style={styles.result}>{this.state.result}</Text>
           </View>
           <View style={styles.row}>
-              <Button text='AC' flexValue='3'></Button>
-              <Button text='/' bgColor='#FFA500' colorText='white'></Button>
+              <Button text='AC' flexValue='3' bgColor='#CCCCCC' onPress={()=>{this.calculation('AC')}}></Button>
+              <Button text='/' bgColor='#FFA500' colorText='white' onPress={()=>{this.calculation('/')}}></Button>
           </View>
           <View style={styles.row}>
-              <Button text='7'></Button>
-              <Button text='8'></Button>
-              <Button text='9'></Button>
-              <Button text='*' bgColor='#FFA500' colorText='white'></Button>
+              <Button text='7' onPress={()=>{this.calculation('7')}}></Button>
+              <Button text='8' onPress={()=>{this.calculation('8')}}></Button>
+              <Button text='9' onPress={()=>{this.calculation('9')}}></Button>
+              <Button text='*' bgColor='#FFA500' colorText='white' onPress={()=>{this.calculation('*')}}></Button>
           </View>
           <View style={styles.row}>
-              <Button text='4'></Button>
-              <Button text='5'></Button>
-              <Button text='6'></Button>
-              <Button text='-' bgColor='#FFA500' colorText='white'></Button>
+              <Button text='4' onPress={()=>{this.calculation('4')}}></Button>
+              <Button text='5' onPress={()=>{this.calculation('5')}}></Button>
+              <Button text='6' onPress={()=>{this.calculation('6')}}></Button>
+              <Button text='-' bgColor='#FFA500' colorText='white' onPress={()=>{this.calculation('-')}}></Button>
           </View>
           <View style={styles.row}>
-              <Button text='1'></Button>
-              <Button text='2'></Button>
-              <Button text='3'></Button>
-              <Button text='+' bgColor='#FFA500' colorText='white'></Button>
+              <Button text='1' onPress={()=>{this.calculation('1')}}></Button>
+              <Button text='2' onPress={()=>{this.calculation('2')}}></Button>
+              <Button text='3' onPress={()=>{this.calculation('3')}}></Button>
+              <Button text='+' bgColor='#FFA500' colorText='white' onPress={()=>{this.calculation('+')}}></Button>
           </View>
           <View style={styles.row}>
-              <Button text='0' flexValue='2'></Button>
-              <Button text=','></Button>
-              <Button text='=' bgColor='#FFA500' colorText='white'></Button>
+              <Button text='0' flexValue='2' onPress={()=>{this.calculation('0')}}></Button>
+              <Button text=',' onPress={()=>{this.calculation('.')}}></Button>
+              <Button text='=' bgColor='#FFA500' colorText='white' onPress={()=>{this.calculation('=')}}></Button>
           </View>
       </View>
     );
@@ -100,5 +126,11 @@ const styles = StyleSheet.create({
   row: {
     flex: 1,
     flexDirection: 'row'
+  },
+  result: {
+    flex: 1,
+    fontSize: 50,
+    textAlign: 'right',
+    padding: 15
   }
 });
